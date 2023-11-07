c22738480638080b003fa93a4e4f80344df809c2
mido1 <mido.15897@gmail.com>
2023-11-07 15:53:47 +0300

    first project - first comment

---
+++ src/pages/Add.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Add = () => {
+    return (
+        <div className="header">
+            Add
+        </div>
+    );
+};
+
+export default Add;
---
+++ src/pages/Edit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Edit = () => {
+    return (
+        <div className="header">
+            Edit
+        </div>
+    );
+};
+
+export default Edit;
