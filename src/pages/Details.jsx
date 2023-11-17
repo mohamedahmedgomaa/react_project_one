156bcee5ed28147236a771401faa835166451652
mido1 <mido.15897@gmail.com>
2023-11-17 14:55:45 +0300

    Add Post Crud -> details fix one

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,17 @@
 import usePostDetails from "../hooks/use-post-details";
 import Loading from "../components/Loading";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
 
 const Details = () => {
+    const dispatch = useDispatch()
     const {loading, error, record} = usePostDetails();
 
+    useEffect(() => {
+        return () => {
+            dispatch({type: "posts/cleanRecord"})
+        }
+    }, [dispatch]);
     return (
         <>
             <Loading loading={loading} error={error}>
